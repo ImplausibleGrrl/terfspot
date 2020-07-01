e53b7c786df3d719c2170ead1604ee57da74d074
Christina Tingyue Lu <luchrist@curry.si.umich.edu>
2020-07-01 18:32:52 -0400

    Initialized project using cookiecutter.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Computationally analyzing and detecting TERFs on Twitter.',
+    author='Christina Lu',
+    license='MIT',
+)
